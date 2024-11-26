e5204a58e00787dd0bad453d1d86a70bf5ae03e8
Raunak Dani <daniraunak22@gmail.com>
2024-11-25 20:02:53 -0500

    Update tp1_team_13.py

--- Team Checkpoint 1/tp1_team_13.py
+++ Team Checkpoint 1/tp1_team_13.py
@@ -1,36 +1,3 @@
-"""
-Course Number: ENGR 13300
-Semester: Fall 2024
-
-Description:
-    Integrated code to extract and display the hidden message in a human-readable format.
-
-Assignment Information:
-    Assignment:     9.2.3 tp1 Team 3
-    Team ID:        LC5 - 13
-    Author:         Rohan Shah, shah1091@purdue.edu; Raunak Dani, dani@purdue.edu, Timothy Jiang, jian1046@purdue.edu, Fabricio Giusti Oliveira Monteiro, fgiustio@purdue.edu
-    Date:           10/01/2024
-
-Contributors:
-    Name, login@purdue [repeat for each]
-
-    My contributor(s) helped me:
-    [ ] understand the assignment expectations without
-        telling me how they will approach it.
-    [ ] understand different ways to think about a solution
-        without helping me plan my solution.
-    [ ] think through the meaning of a specific error or
-        bug present in my code without looking at my code.
-    Note that if you helped somebody else with their code, you
-    have to list that person as a contributor here as well.
-
-Academic Integrity Statement:
-    I have not used source code obtained from any unauthorized
-    source, either modified or unmodified; nor have I provided
-    another student access to my code.  The project I am
-    submitting is my own original work.
-"""
-
 #Importing the required libraries
 import numpy as np
 import matplotlib.pyplot as plt
