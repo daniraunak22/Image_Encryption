70030473749a71a3a5984053b2e731b5dee20ff6
Raunak Dani <daniraunak22@gmail.com>
2024-11-25 20:20:53 -0500

    Update tp2_team_2_13.py

--- Checkpoint 2/tp2_team_2_13.py
+++ Checkpoint 2/tp2_team_2_13.py
@@ -1,34 +1,6 @@
 """
-Course Number: ENGR 13300
-Semester: Fall 2024
-
 Description:
     Uses the Vigenère cipher to encrypt a string. Specifically, a word can be entered, and then encrypted using a key word. This shifts the indexes, outputting an encrypted word.
-
-Assignment Information:
-    Assignment:     tp2 task 2
-    Team ID:        LC5 - 13 
-    Author:         Rohan Shah, shah1091@purdue.edu; Raunak Dani, dani@purdue.edu, Timothy Jiang, jian1046@purude.edu, Faricio Giusti Oliveira Monteiro, fgiustio@purdue.edu
-    Date:           10/08/2024
-
-Contributors:
-    Name, login@purdue [repeat for each]
-
-    My contributor(s) helped me:
-    [ ] understand the assignment expectations without
-        telling me how they will approach it.
-    [ ] understand different ways to think about a solution
-        without helping me plan my solution.
-    [ ] think through the meaning of a specific error or
-        bug present in my code without looking at my code.
-    Note that if you helped somebody else with their code, you
-    have to list that person as a contributor here as well.
-
-Academic Integrity Statement:
-    I have not used source code obtained from any unauthorized
-    source, either modified or unmodified; nor have I provided
-    another student access to my code.  The project I am
-    submitting is my own original work.
 """
  
 def vigenere_encrypt(text, key):
